c85b65f401d8f81c1832af96e4e4b523801d9ad8
stackotter <stackotter@stackotter.dev>
2024-02-05 18:14:14 +1000

    Remove unnecessary first rasterization phase

--- RelativisticRenderer.swiftpm/RelativisticRenderer.swift
+++ RelativisticRenderer.swiftpm/RelativisticRenderer.swift
@@ -1,18 +1,9 @@
 import MetalKit
 
 struct RelativisticRenderer: Renderer {
-    var vertices: [SIMD3<Float>] = [
-        [0, 1, 0],
-        [1, -1, 0],
-        [-1, -1, 0]
-    ]
-
     let library: any MTLLibrary
-    let renderPipelineState: any MTLRenderPipelineState
-    let vertexBuffer: any MTLBuffer
     let computePipelineState: any MTLComputePipelineState
     let blitPipelineState: any MTLRenderPipelineState
-    var backgroundTexture: (any MTLTexture)?
     var computeOutputTexture: (any MTLTexture)?
     
     init(device: MTLDevice, commandQueue: MTLCommandQueue) throws {
@@ -23,25 +14,6 @@ struct RelativisticRenderer: Renderer {
                 #include <metal_stdlib>
                 
                 using namespace metal;
-                
-                typedef struct {
-                    float3 position;
-                } InVertex;
-                
-                typedef struct {
-                    float4 position [[position]];
-                } OutVertex;
-                
-                vertex OutVertex vertexFunction(constant InVertex *vertices [[buffer(0)]],
-                                                uint vertexId [[vertex_id]]) {
-                    InVertex in = vertices[vertexId];
-                    OutVertex out = { .position = float4(in.position, 1) };
-                    return out;
-                }
-                
-                fragment float4 fragmentFunction(OutVertex in [[stage_in]]) {
-                    return float4(1, 0, 0, 1);
-                }
                 
                 constexpr sampler textureSampler (mag_filter::linear, min_filter::linear);
                 
@@ -57,11 +29,10 @@ struct RelativisticRenderer: Renderer {
                     }
                 }
                 
-                kernel void computeFunction(texture2d<float, access::sample> inTexture [[texture(0)]],
-                                       texture2d<float, access::write> outTexture [[texture(1)]],
-                                       uint2 gid [[thread_position_in_grid]]) {
-                    float textureWidth = (float)inTexture.get_width();
-                    float textureHeight = (float)inTexture.get_height();
+                kernel void computeFunction(texture2d<float, access::write> outTexture [[texture(0)]],
+                                            uint2 gid [[thread_position_in_grid]]) {
+                    float textureWidth = (float)outTexture.get_width();
+                    float textureHeight = (float)outTexture.get_height();
                     
                     // A ray from the camera representing the direction that light must come from to
                     // contribute to the current pixel (we trace this ray backwards).
@@ -165,32 +136,6 @@ struct RelativisticRenderer: Renderer {
             throw SimpleError("Failed to compile shaders: \(error)")
         }
         
-        guard
-            let vertexFunction = library.makeFunction(name: "vertexFunction"),
-            let fragmentFunction = library.makeFunction(name: "fragmentFunction")
-        else {
-            throw SimpleError("Failed to get vertex function or fragment function")
-        }
-        
-        let pipelineDescriptor = MTLRenderPipelineDescriptor()
-        pipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
-        pipelineDescriptor.vertexFunction = vertexFunction
-        pipelineDescriptor.fragmentFunction = fragmentFunction
-        
-        do {
-            renderPipelineState = try device.makeRenderPipelineState(descriptor: pipelineDescriptor)
-        } catch {
-            throw SimpleError("Failed to make render pipeline state: \(error)")
-        }
-        
-        let bytes = MemoryLayout<SIMD3<Float>>.stride * vertices.count
-        
-        guard let buffer = device.makeBuffer(length: bytes) else {
-            throw SimpleError("Failed to make vertex buffer")
-        }
-        buffer.contents().copyMemory(from: &vertices, byteCount: bytes)
-        vertexBuffer = buffer
-        
         guard let computeFunction = library.makeFunction(name: "computeFunction") else {
             throw SimpleError("Failed to get compute function")
         }
@@ -221,30 +166,14 @@ struct RelativisticRenderer: Renderer {
     }
     
     mutating func render(view: MTKView, device: MTLDevice, renderPassDescriptor: MTLRenderPassDescriptor, drawable: MTLDrawable, commandBuffer: MTLCommandBuffer) throws {
-        let (backgroundTexture, computeOutputTexture) = try updateTextures(device, view)
-        
-        let backgroundRenderPassDescriptor = MTLRenderPassDescriptor()
-        backgroundRenderPassDescriptor.colorAttachments[0].texture = backgroundTexture
-        backgroundRenderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0, green: 0, blue: 1, alpha: 1)
-        backgroundRenderPassDescriptor.colorAttachments[0].loadAction = .clear
-        backgroundRenderPassDescriptor.colorAttachments[0].storeAction = .store
-        
-        guard let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: backgroundRenderPassDescriptor) else {
-            throw SimpleError("Failed to make render command encoder")
-        }
-        
-        renderEncoder.setRenderPipelineState(renderPipelineState)
-        renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
-        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertices.count)
-        renderEncoder.endEncoding()
+        let computeOutputTexture = try updateOutputTexture(device, view)
         
         guard let computeEncoder = commandBuffer.makeComputeCommandEncoder() else {
             throw SimpleError("Failed to make compute command encoder")
         }
         
         computeEncoder.setComputePipelineState(computePipelineState)
-        computeEncoder.setTexture(backgroundTexture, index: 0)
-        computeEncoder.setTexture(computeOutputTexture, index: 1)
+        computeEncoder.setTexture(computeOutputTexture, index: 0)
         computeEncoder.dispatchThreadgroups(
             MTLSize(width: Int(view.drawableSize.width + 15) / 16, height: Int(view.drawableSize.height + 15) / 16, depth: 1),
             threadsPerThreadgroup: MTLSize(width: 16, height: 16, depth: 1)
@@ -264,12 +193,9 @@ struct RelativisticRenderer: Renderer {
         commandBuffer.commit()
     }
     
-    mutating func updateTextures(_ device: MTLDevice, _ view: MTKView) throws -> (backgroundTexture: MTLTexture, computeOutputTexture: MTLTexture) {
+    mutating func updateOutputTexture(_ device: MTLDevice, _ view: MTKView) throws -> MTLTexture {
         guard
-            let backgroundTexture = self.backgroundTexture,
             let computeOutputTexture = self.computeOutputTexture,
-            backgroundTexture.width == computeOutputTexture.width,
-            backgroundTexture.height == computeOutputTexture.height,
             computeOutputTexture.width == Int(view.drawableSize.width),
             computeOutputTexture.height == Int(view.drawableSize.height)
         else {
@@ -277,23 +203,16 @@ struct RelativisticRenderer: Renderer {
             textureDescriptor.width = Int(view.drawableSize.width)
             textureDescriptor.height = Int(view.drawableSize.height)
             textureDescriptor.pixelFormat = .bgra8Unorm
-            textureDescriptor.usage = [.shaderRead, .renderTarget]
-            
-            guard let backgroundTexture = device.makeTexture(descriptor: textureDescriptor) else {
-                throw SimpleError("Failed to make background render target")
-            }
-            self.backgroundTexture = backgroundTexture
-            
             textureDescriptor.usage = [.shaderRead, .shaderWrite]
             guard let computeOutputTexture = device.makeTexture(descriptor: textureDescriptor) else {
                 throw SimpleError("Failed to make compute shader output texture")
             }
             self.computeOutputTexture = computeOutputTexture
             
-            return (backgroundTexture, computeOutputTexture)
+            return computeOutputTexture
         }
         
-        return (backgroundTexture, computeOutputTexture)
+        return computeOutputTexture
     }
 }
 
